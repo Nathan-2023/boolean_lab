ab0db363e734beffa1870b29927ffa21637688b6
Nathan-023 <nathanli2023@lee-sfaa.com>
2022-01-27 23:00:46 +0800

    boolean_lab_nathan

--- comparisons.py
+++ comparisons.py
@@ -4,32 +4,32 @@
 def greater_than_one():
     # this function should return True if the input is greater than 1 (should work for floats and int)
     input_number = input("Pick a number, any number! ")
-    return #<
+    return float(input_number) > 1
 
 
 def you_are_number_one_alphabetically():
     # this function should return true if your name is alphabetically first
     # Hint: you can use the .upper() or .lower() functions on a string to return all lowercase or uppercase
-    your_alien_classmate = "Zzanqfdort"
-    your_name = input("What's your name? ")
-    return #<
+    your_alien_classmate = "Zzanqfdort".upper()
+    your_name = input("What's your name? ").upper()
+    return your_name.upper() < your_alien_classmate.upper()
 
 
 def are_you_voting_age():
     # this function should return true if you are 18 or older
     your_age = input("What's your age? ")
-    return #>=
+    return int(your_age) > 18
 
 
 def were_not_so_different_you_and_i():
     # this function should return true if the two inputs are the same
     input_1 = input("Enter a value: ")
     input_2 = input("Enter another value: ")
-    return
+    return input_1 == input_2
 
 
 def we_are_not_the_same():
     # this function should return true if the input is different than the variable
     pi = 3.14159
     input_pi = input("Do you like pi? ")
-    return
+    return float(input_pi) != float(pi)
